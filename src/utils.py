de6e7e5059927744b1f7ba3865defb5ea168882b
Freddie Bickford Smith <30656062+fbickfordsmith@users.noreply.github.com>
2024-05-08 11:09:14 +0100

    Tidy up src/

--- src/utils.py
+++ src/utils.py
@@ -5,19 +5,20 @@
 # LICENSE file in the root directory of this source tree.
 #
 
-import os
 import math
+import os
+from logging import getLogger
+
 import torch
 import torch.distributed as dist
 
-from logging import getLogger
 
 logger = getLogger()
 
 
 def gpu_timer(closure, log_timings=True):
-    """ Helper to time gpu-time to execute closure() """
-    elapsed_time = -1.
+    """Helper to time gpu-time to execute closure()"""
+    elapsed_time = -1.0
     if log_timings:
         start = torch.cuda.Event(enable_timing=True)
         end = torch.cuda.Event(enable_timing=True)
@@ -34,47 +35,35 @@ def gpu_timer(closure, log_timings=True):
 
 
 def init_distributed(port=40111, rank_and_world_size=(None, None)):
-
     if dist.is_available() and dist.is_initialized():
         return dist.get_world_size(), dist.get_rank()
 
     rank, world_size = rank_and_world_size
-    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ["MASTER_ADDR"] = "localhost"
 
     if (rank is None) or (world_size is None):
         try:
-            world_size = int(os.environ['SLURM_NTASKS'])
-            rank = int(os.environ['SLURM_PROCID'])
-            os.environ['MASTER_ADDR'] = os.environ['HOSTNAME']
+            world_size = int(os.environ["SLURM_NTASKS"])
+            rank = int(os.environ["SLURM_PROCID"])
+            os.environ["MASTER_ADDR"] = os.environ["HOSTNAME"]
         except Exception:
-            logger.info('SLURM vars not set (distributed training not available)')
+            logger.info("SLURM vars not set (distributed training not available)")
             world_size, rank = 1, 0
             return world_size, rank
 
     try:
-        os.environ['MASTER_PORT'] = str(port)
-        torch.distributed.init_process_group(
-            backend='nccl',
-            world_size=world_size,
-            rank=rank)
+        os.environ["MASTER_PORT"] = str(port)
+        torch.distributed.init_process_group(backend="nccl", world_size=world_size, rank=rank)
     except Exception:
         world_size, rank = 1, 0
-        logger.info('distributed training not available')
+        logger.info("distributed training not available")
 
     return world_size, rank
 
 
 class WarmupCosineSchedule(object):
-
     def __init__(
-        self,
-        optimizer,
-        warmup_steps,
-        start_lr,
-        ref_lr,
-        T_max,
-        last_epoch=-1,
-        final_lr=0.
+        self, optimizer, warmup_steps, start_lr, ref_lr, T_max, last_epoch=-1, final_lr=0.0
     ):
         self.optimizer = optimizer
         self.start_lr = start_lr
@@ -82,7 +71,7 @@ class WarmupCosineSchedule(object):
         self.final_lr = final_lr
         self.warmup_steps = warmup_steps
         self.T_max = T_max - warmup_steps
-        self._step = 0.
+        self._step = 0.0
 
     def step(self):
         self._step += 1
@@ -92,34 +81,32 @@ class WarmupCosineSchedule(object):
         else:
             # -- progress after warmup
             progress = float(self._step - self.warmup_steps) / float(max(1, self.T_max))
-            new_lr = max(self.final_lr,
-                         self.final_lr + (self.ref_lr - self.final_lr) * 0.5 * (1. + math.cos(math.pi * progress)))
+            new_lr = max(
+                self.final_lr,
+                self.final_lr
+                + (self.ref_lr - self.final_lr) * 0.5 * (1.0 + math.cos(math.pi * progress)),
+            )
 
         for group in self.optimizer.param_groups:
-            group['lr'] = new_lr
+            group["lr"] = new_lr
 
         return new_lr
 
 
 class CosineWDSchedule(object):
-
-    def __init__(
-        self,
-        optimizer,
-        ref_wd,
-        T_max,
-        final_wd=0.
-    ):
+    def __init__(self, optimizer, ref_wd, T_max, final_wd=0.0):
         self.optimizer = optimizer
         self.ref_wd = ref_wd
         self.final_wd = final_wd
         self.T_max = T_max
-        self._step = 0.
+        self._step = 0.0
 
     def step(self):
         self._step += 1
         progress = self._step / self.T_max
-        new_wd = self.final_wd + (self.ref_wd - self.final_wd) * 0.5 * (1. + math.cos(math.pi * progress))
+        new_wd = self.final_wd + (self.ref_wd - self.final_wd) * 0.5 * (
+            1.0 + math.cos(math.pi * progress)
+        )
 
         if self.final_wd <= self.ref_wd:
             new_wd = max(self.final_wd, new_wd)
@@ -127,29 +114,28 @@ class CosineWDSchedule(object):
             new_wd = min(self.final_wd, new_wd)
 
         for group in self.optimizer.param_groups:
-            if ('WD_exclude' not in group) or not group['WD_exclude']:
-                group['weight_decay'] = new_wd
+            if ("WD_exclude" not in group) or not group["WD_exclude"]:
+                group["weight_decay"] = new_wd
         return new_wd
 
 
 class CSVLogger(object):
-
     def __init__(self, fname, *argv):
         self.fname = fname
         self.types = []
         # -- print headers
-        with open(self.fname, '+a') as f:
+        with open(self.fname, "+a") as f:
             for i, v in enumerate(argv, 1):
                 self.types.append(v[0])
                 if i < len(argv):
-                    print(v[1], end=',', file=f)
+                    print(v[1], end=",", file=f)
                 else:
-                    print(v[1], end='\n', file=f)
+                    print(v[1], end="\n", file=f)
 
     def log(self, *argv):
-        with open(self.fname, '+a') as f:
+        with open(self.fname, "+a") as f:
             for i, tv in enumerate(zip(self.types, argv), 1):
-                end = ',' if i < len(argv) else '\n'
+                end = "," if i < len(argv) else "\n"
                 print(tv[0] % tv[1], end=end, file=f)
 
 
@@ -162,8 +148,8 @@ class AverageMeter(object):
     def reset(self):
         self.val = 0
         self.avg = 0
-        self.max = float('-inf')
-        self.min = float('inf')
+        self.max = float("-inf")
+        self.min = float("inf")
         self.sum = 0
         self.count = 0
 
@@ -177,14 +163,9 @@ class AverageMeter(object):
 
 
 class AllGather(torch.autograd.Function):
-
     @staticmethod
     def forward(ctx, x):
-        if (
-            dist.is_available()
-            and dist.is_initialized()
-            and (dist.get_world_size() > 1)
-        ):
+        if dist.is_available() and dist.is_initialized() and (dist.get_world_size() > 1):
             outputs = [torch.zeros_like(x) for _ in range(dist.get_world_size())]
             dist.all_gather(outputs, x)
             return torch.cat(outputs, 0)
@@ -192,11 +173,7 @@ class AllGather(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grads):
-        if (
-            dist.is_available()
-            and dist.is_initialized()
-            and (dist.get_world_size() > 1)
-        ):
+        if dist.is_available() and dist.is_initialized() and (dist.get_world_size() > 1):
             s = (grads.shape[0] // dist.get_world_size()) * dist.get_rank()
             e = (grads.shape[0] // dist.get_world_size()) * (dist.get_rank() + 1)
             grads = grads.contiguous()
@@ -206,14 +183,9 @@ class AllGather(torch.autograd.Function):
 
 
 class AllReduceSum(torch.autograd.Function):
-
     @staticmethod
     def forward(ctx, x):
-        if (
-            dist.is_available()
-            and dist.is_initialized()
-            and (dist.get_world_size() > 1)
-        ):
+        if dist.is_available() and dist.is_initialized() and (dist.get_world_size() > 1):
             x = x.contiguous()
             dist.all_reduce(x)
         return x
@@ -224,14 +196,9 @@ class AllReduceSum(torch.autograd.Function):
 
 
 class AllReduce(torch.autograd.Function):
-
     @staticmethod
     def forward(ctx, x):
-        if (
-            dist.is_available()
-            and dist.is_initialized()
-            and (dist.get_world_size() > 1)
-        ):
+        if dist.is_available() and dist.is_initialized() and (dist.get_world_size() > 1):
             x = x.contiguous() / dist.get_world_size()
             dist.all_reduce(x)
         return x
@@ -246,7 +213,7 @@ def _no_grad_trunc_normal_(tensor, mean, std, a, b):
     # Method based on https://people.sc.fsu.edu/~jburkardt/presentations/truncated_normal.pdf
     def norm_cdf(x):
         # Computes standard normal cumulative distribution function
-        return (1. + math.erf(x / math.sqrt(2.))) / 2.
+        return (1.0 + math.erf(x / math.sqrt(2.0))) / 2.0
 
     with torch.no_grad():
         # Values are generated by using a truncated uniform distribution and
@@ -264,7 +231,7 @@ def _no_grad_trunc_normal_(tensor, mean, std, a, b):
         tensor.erfinv_()
 
         # Transform to proper mean, std
-        tensor.mul_(std * math.sqrt(2.))
+        tensor.mul_(std * math.sqrt(2.0))
         tensor.add_(mean)
 
         # Clamp to ensure it's in the proper range
@@ -272,7 +239,7 @@ def _no_grad_trunc_normal_(tensor, mean, std, a, b):
         return tensor
 
 
-def trunc_normal_(tensor, mean=0., std=1., a=-2., b=2.):
+def trunc_normal_(tensor, mean=0.0, std=1.0, a=-2.0, b=2.0):
     # type: (Tensor, float, float, float, float) -> Tensor
     return _no_grad_trunc_normal_(tensor, mean, std, a, b)
 
@@ -282,13 +249,13 @@ def grad_logger(named_params):
     stats.first_layer = None
     stats.last_layer = None
     for n, p in named_params:
-        if (p.grad is not None) and not (n.endswith('.bias') or len(p.shape) == 1):
+        if (p.grad is not None) and not (n.endswith(".bias") or len(p.shape) == 1):
             grad_norm = float(torch.norm(p.grad.data))
             stats.update(grad_norm)
-            if 'qkv' in n:
+            if "qkv" in n:
                 stats.last_layer = grad_norm
                 if stats.first_layer is None:
                     stats.first_layer = grad_norm
     if stats.first_layer is None or stats.last_layer is None:
-        stats.first_layer = stats.last_layer = 0.
+        stats.first_layer = stats.last_layer = 0.0
     return stats
